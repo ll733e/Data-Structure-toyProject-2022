fddc8aef1676c25e060563ddf741808e3789de7b
Ubuntu <ubuntu@ip-172-31-43-147.ap-northeast-2.compute.internal>
2022-06-01 05:51:06 +0000

    로그인 구현 중, delUser 헬프콜

--- src/lib_login/IAM.c
+++ src/lib_login/IAM.c
@@ -3,31 +3,46 @@
 #include "..//lib_json//parson.h"
 #include "IAM.h"
 
-void addUser(char* ID, char* PW) {
-/*
-	JSON_Value		*rootValue = json_parse_file("account.json");
+void addUser(double ID, double PW) {
+	JSON_Value		*rootValue = json_parse_file("..//..//user//userSample.json");
 	JSON_Object 	*rootObject = json_value_get_object(rootValue);
 
 	//memset(ID_encrpyt, 0, sizeof(ID_encrpyt));
 	//memset(PW_encrpyt, 0, sizeof(PW_encrpyt));
 
-	read(0, ID, 12);//ID는 일단 학번으로 생각해서 10자리 예상
-	read(0, PW, 12);//최대 12자리까지만 가능하도록 예상
+	//read(0, ID, 12);//ID는 일단 학번으로 생각해서 10자리 예상
+	//read(0, PW, 12);//최대 12자리까지만 가능하도록 예상
 
 	//SHA256_Encrpyt(ID, strlen(ID), ID_encrpyt);
 	//SHA256_Encrpyt(PW, strlen(PW), PW_encrpyt);
 
-	json_object_set_string(rootObject, "Title", "Account");
-	json_object_set_string(rootObject, "ID", ID);
+	//json_object_set_string(rootObject, "Title", "Account");
+	json_object_set_number(rootObject, "ID", ID);
+	json_object_set_number(rootObject, "hash", PW);
 
-	json_serialize_to_file_pretty(rootValue, "account.json");
+	json_object_set_value(rootObject, "rent", json_value_init_array());
+	JSON_Array *rent = json_object_get_array(rootObject, "rent");
+	json_object_set_value(rootObject, "waiting", json_value_init_array());
+	JSON_Array *waiting = json_object_get_array(rootObject, "waiting");
+	json_array_append_string(rent,"book1");
+	json_array_append_string(waiting,"wait_book1");
+
+	json_serialize_to_file_pretty(rootValue, "..//..//user//userSample.json");
 	json_value_free(rootValue);
-
-	*/
 }
-void            delUser();      // 사용자 탈퇴 OR 사용자 자격 박탈
-void            prtUser();      // 사용자 정보 출력 (대출 권수, 대출 목록)
 
+void delUser();      // 사용자 탈퇴 OR 사용자 자격 박탈
+void delUser(double ID) {
+
+	JSON_Value		*rootValue = json_parse_file("..//..//user//userSample.json");
+	JSON_Object 	*rootObject = json_value_get_object(rootValue);
+
+	
+}
+void prtUser();      // 사용자 정보 출력 (대출 권수, 대출 목록)
+void prtUser() {
+
+}
 /*          Indentification & Authentication        */
 void            logUser();      // 사용자 로그인 기능 함수
 unsigned int    getUser();      // 사용자 정보 가져오기
