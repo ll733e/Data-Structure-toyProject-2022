83014773356d5eb78acfc6eb342da3a113c7c7ea
Arizen <dkflwps@naver.com>
2022-06-21 04:40:09 +0900

    SocketCore Base

--- src/socket/socketCore.c
+++ src/socket/socketCore.c
@@ -4,9 +4,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-void error_handling(char * message);
 
-int main(int argc, char* argv[]) {
+int tcpListen(int argc, char* argv[]) {
     int     serv_sock;
     int     clnt_sock;
    
@@ -15,38 +14,35 @@ int main(int argc, char* argv[]) {
     socklen_t clnt_addr_size;
    
     serv_sock = socket(PF_INET, SOCK_STREAM, 0); 
-    if(serv_sock == -1)     error_handling("socket error");
+    if(serv_sock == -1) puts("Socket Error");
     
-    //주소를 초기화한 후 IP주소와 포트 지정
     memset(&serv_addr, 0, sizeof(serv_addr)); 
     serv_addr.sin_family        = AF_INET;
     serv_addr.sin_addr.s_addr   = htonl(INADDR_ANY);
-    serv_addr.sin_port          = htons(atoi(argv[1]));
+    serv_addr.sin_port          = htons(atoi(8888));
     
-    //소켓과 서버 주소를 바인딩
-    if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
-                                        error_handling("bind error");
-    
-    //연결 대기열 5개 생성 
-    if(listen(serv_sock, 5) == -1)      error_handling("listen error");
-    
-    //클라이언트로부터 요청이 오면 연결 수락
-    clnt_addr_size = sizeof(clnt_addr);
-    clnt_sock=accept(serv_sock, (struct sockaddr*) &clnt_addr, &clnt_addr_size);
-    if(clnt_sock == -1)     error_handling("accept error");
+    if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1) puts("Bind Error");
+    if(listen(serv_sock, 5) == -1) puts("Listen Error");
 
-    /*-----데이터 전송-----*/
-    char msg[] = "Hello this is server!\n";
-    write(clnt_sock, msg, sizeof(msg));
-    
-    //소켓들 닫기
+    clnt_addr_size = sizeof(clnt_addr);
+
+    char buf[2048];
+    int buf_len;
+
+    while (1)
+    {
+        clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_addr, &clnt_addr_size);
+        if(clnt_sock == -1) puts("Accept Error");
+        memset(buf, 0, sizeof(buf));
+        buf_len = read(serv_sock, buf, sizeof(buf)-1);
+        if(buf_len == -1) puts("Read Error");
+
+        //Do somthing Here
+
+        write(clnt_sock, res, sizeof(res));
+    }
     close(clnt_sock);
     close(serv_sock);
     
     return 0;
-}
-void error_handling(char *message) {
-    fputs(message,stderr);
-    fputc('\n', stderr);
-    exit(1);
 }
